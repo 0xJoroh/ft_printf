d13edc7c52a6ab7bc6f8fa3a795977bb42627aa2
Mohamed Ait si m hand <mait-si-@e2r5p2.1337.ma>
2019-11-17 12:33:38 +0100

    s-push

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: mait-si- <mait-si-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/13 18:03:44 by mait-si-          #+#    #+#             */
-/*   Updated: 2019/11/17 12:28:36 by mait-si-         ###   ########.fr       */
+/*   Updated: 2019/11/17 12:33:15 by mait-si-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 int		main(void)
 {
-	printf("\t\t\t[%d]\n", ft_printf("[%s - %c - %d - %i - %x - %X]", "hello", 'c', 12, 96, 5699, 5699));
-	printf("\t\t\t[%d]", printf("[%s - %c - %d - %i - %x - %X]", "hello", 'c', 12, 96, 566699, 566699));
+	// printf("\t\t\t[%d]\n", ft_printf("[%s - %c - %d - %i - %x - %X]", "hello", 'c', 12, 96, 5699, 5699));
+	// printf("\t\t\t[%d]", printf("[%s - %c - %d - %i - %x - %X]", "hello", 'c', 12, 96, 566699, 566699));
 	// printf("%s", ft_strlower("1bJ324"));
 	return (0);
 }
