baa9905036936895c4e4bfee5693b1c744dd36e6
Mohamed Ait si m hand <mait-si-@e2r5p2.1337.ma>
2019-11-17 16:45:36 +0100

    comment

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: mait-si- <mait-si-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/13 18:03:44 by mait-si-          #+#    #+#             */
-/*   Updated: 2019/11/17 16:32:13 by mait-si-         ###   ########.fr       */
+/*   Updated: 2019/11/17 16:44:16 by mait-si-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,16 @@
 
 int		main(void)
 {
-	int x;
-	int *ptr;
+	// int x;
+	// int *ptr;
 
-	ptr = &x;
-	printf("\t\t\t[%d]\n", ft_printf("[%s - %c - %d - %i - %x - %X - %p]", "hello", 'c', 12, 96, -566699, 56663899, &x));
-	printf("\t\t\t[%d]", printf("[%s - %c - %d - %i - %x - %X - %p]", "hello", 'c', 12, 96, -566699, 56663899, &x));
+	// ptr = &x;
+	// printf("\t\t\t[%d]\n", ft_printf("[%s - %c - %d - %i - %u - %x - %X - %p]", "hello", 'c', 12, 96, 4294967294, -566699, 56663899, &x));
+	// printf("\t\t\t[%d]",      printf("[%s - %c - %d - %i - %u - %x - %X - %p]", "hello", 'c', 12, 96, 4194967294, -566699, 56663899, &x));
+	// int a = 5;
+    // check the memory address
+    // printf("memory address = %d\n", &a); /* wrong */
+    // printf("memory address = %u\n", &a); /* wrong */
+    // printf("memory address = %p\n", (void*)&a); /* right */
 	return (0);
 }
