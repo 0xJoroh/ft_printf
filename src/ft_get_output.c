af60b5a491ac9e9fafbda71a82dd10c120a95cc3
Mohamed Ait si m hand <mait-si-@e2r5p2.1337.ma>
2019-11-20 21:06:58 +0100

    comment

--- src/ft_get_output.c
+++ src/ft_get_output.c
@@ -6,20 +6,11 @@
 /*   By: mait-si- <mait-si-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/19 02:22:26 by mait-si-          #+#    #+#             */
-/*   Updated: 2019/11/20 18:51:57 by mait-si-         ###   ########.fr       */
+/*   Updated: 2019/11/20 21:06:56 by mait-si-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../ft_printf.h"
-
-static int				ft_check_conv(const char **format)
-{
-	while (**format == '%')
-		format++;
-	if (ft_isalpha(**format) && ft_isconv(**(format + 1)))
-		return (1);
-	return (0);
-}
 
 static void		ft_init_flag(t_flag *flag)
 {
@@ -30,14 +21,25 @@ static void		ft_init_flag(t_flag *flag)
 	flag->content = 0;
 }
 
-void			ft_put_struct(t_flag flag)
+// void			ft_put_struct(t_flag flag)
+// {
+// 	ft_putstr("\n\tconversion\t:\t");ft_putchar(flag.conv);
+// 	ft_putstr("\n\twidth\t\t:\t");ft_putnbr(flag.width);
+// 	ft_putstr("\n\toption\t\t:\t");ft_putchar(flag.opt);
+// 	ft_putstr("\n\tprecesion\t:\t");ft_putnbr(flag.prec);
+// 	ft_putstr("\n\tcontent\t\t:\t");ft_putstr((char*)flag.content);
+// 	ft_putstr("\n");
+// }
+
+static int		ft_get_parsed(t_flag flag, const char *format, va_list list)
 {
-	ft_putstr("\n\tconversion\t:\t");ft_putchar(flag.conv);
-	ft_putstr("\n\twidth\t\t:\t");ft_putnbr(flag.width);
-	ft_putstr("\n\toption\t\t:\t");ft_putchar(flag.opt);
-	ft_putstr("\n\tprecesion\t:\t");ft_putnbr(flag.prec);
-	ft_putstr("\n\tcontent\t\t:\t");ft_putstr((char*)flag.content);
-	ft_putstr("\n");
+	char *str;
+
+	ft_init_flag(&flag);
+	ft_set_flag(format, &flag, list);
+	ft_put_struct(flag);
+	ft_putstr(str = ft_parse_flag(&flag));
+	return ((!ft_strlen(str) ? 1 : ft_strlen(str)));
 }
 
 int				ft_get_output(const char *format, va_list list)
@@ -47,25 +49,19 @@ int				ft_get_output(const char *format, va_list list)
 	t_flag		flag;
 
 	counter = 0;
-	// if (*(format + 1) == '\0')
-	// 	break ;
 	while (*format)
 	{
-		if (*format == '%')
-			if (*(format + 1) == '\0')
+		if (*format++ == '%')
+			if (*format == '%')
+				counter += ft_putchar(*format);
+			else if (*(format + 1) == '\0')
 				break ;
-			else if (ft_check_conv(&format))
+			else
 			{
-				ft_init_flag(&flag);
-				ft_set_flag(format, &flag, list);
-				ft_put_struct(flag);
-				// ft_putstr(str = ft_parse_flag(&flag));
-				counter += (!ft_strlen(str) ? 1 : ft_strlen(str));
+				counter += ft_get_parsed(flag, format, list);
 				while (*format != flag.conv)
 					format++;
 			}
-			else
-				counter += ft_putchar(*format);
 		else
 			counter += ft_putchar(*format);
 		format++;
