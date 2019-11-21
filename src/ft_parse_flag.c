b1476c8b5018ed51c50fd9b5520f9238411824b6
Mohamed Ait si m hand <mait-si-@e2r5p2.1337.ma>
2019-11-21 23:58:39 +0100

    comment

--- src/ft_parse_flag.c
+++ src/ft_parse_flag.c
@@ -6,30 +6,12 @@
 /*   By: mait-si- <mait-si-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/19 17:54:11 by mait-si-          #+#    #+#             */
-/*   Updated: 2019/11/21 23:57:47 by mait-si-         ###   ########.fr       */
+/*   Updated: 2019/11/21 23:58:35 by mait-si-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
 #include "../ft_printf.h"
-
-// static char			*ft_flag_width(t_flag *flag)
-// {
-// 	char	*str;
-// 	int		i;
-// 	int		len;
-
-// 	i = 0;
-// 	len = (flag->width > 0 ? flag->width : flag->width * -1);
-// 	str = malloc(len + 1);
-// 	if (flag->width > 0)
-// 	{
-// 		while (i < (len - ft_numlen(num)))
-// 			str[i++] = ' ';
-// 		ft_strcat(str, ft_itoa(num));
-// 	}
-// 	return (str);
-// }
 
 char				*ft_set_width(t_flag *flag)
 {
